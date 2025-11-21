e36867097f0e4b362d5dd187f97bf9b61bdcd3d3
headEmpty <1393477335@qq.com>
2025-11-21 17:36:50 +0800

    feat(轮播图布局完善):新增搜索及轮播跳转控件

--- lib/pages/Index/components/home_slider.dart
+++ lib/pages/Index/components/home_slider.dart
@@ -1,22 +1,132 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/viewModels/index.dart';
 
 // 轮播图组件
 class HomeSlider extends StatefulWidget {
-  HomeSlider({Key? key}) : super(key: key);
+  final List<BannerItem> bannerList;
+  HomeSlider({Key? key, required this.bannerList}) : super(key: key);
 
   @override
   _HomeSliderState createState() => _HomeSliderState();
 }
 
 class _HomeSliderState extends State<HomeSlider> {
+  int _currentIndex = 0;
+  final CarouselSliderController _carouselController =
+      CarouselSliderController();
+  // 生成搜索栏
+  Widget _getSeachBar() {
+    return Positioned(
+      top: 20,
+      left: 20,
+      right: 20,
+      child: Container(
+        height: 40,
+        alignment: Alignment.center,
+        child: TextField(
+          style: TextStyle(color: Colors.white, fontSize: 16),
+          decoration: InputDecoration(
+            // 背景颜色
+            filled: true,
+            fillColor: const Color.fromARGB(0, 80, 80, 80),
+            // 默认状态下的边框
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(
+                color: Colors.red, // 红色边框
+                width: 2.0,
+              ),
+            ),
+
+            // 聚焦状态下的边框
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide(
+                color: Colors.green, // 绿色边框
+                width: 2.0,
+              ),
+            ),
+
+            hintText: '搜索',
+            // 搜索图标
+            prefixIcon: Icon(
+              Icons.search,
+              color: const Color.fromARGB(255, 196, 11, 11),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 生成banner列表
+  Widget _getBannerList() {
+    return CarouselSlider(
+      carouselController: _carouselController,
+      items: widget.bannerList.map((item) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Image.network(
+              item.imgUrl!,
+              fit: BoxFit.cover,
+              // 图片宽度占满屏幕
+              width: MediaQuery.of(context).size.width,
+            );
+          },
+        );
+      }).toList(),
+      options: CarouselOptions(
+        initialPage: _currentIndex,
+        height: 300,
+        autoPlay: true,
+        aspectRatio: 16 / 9,
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enableInfiniteScroll: true,
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        viewportFraction: 1,
+        onPageChanged: (index, reason) {
+          _currentIndex = index;
+          setState(() {});
+        },
+      ),
+    );
+  }
+
+  // 生成轮播图指示器控件
+  Widget _getIndicator() {
+    return Positioned(
+      bottom: 10,
+      left: 0,
+      right: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(widget.bannerList.length, (index) {
+          return GestureDetector(
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              width: _currentIndex == index ? 30 : 10,
+              height: 4,
+              margin: EdgeInsets.symmetric(horizontal: 4),
+              decoration: BoxDecoration(
+                color: _currentIndex == index
+                    ? const Color.fromARGB(255, 20, 201, 125)
+                    : Colors.grey,
+              ),
+            ),
+            onTap: () {
+              _carouselController.animateToPage(index);
+              _currentIndex = index;
+              setState(() {});
+            },
+          );
+        }).toList(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 300,
-      alignment: Alignment.center,
-      color: Colors.red,
-      child: Text('轮播图'),
-    );
+    return Stack(children: [_getBannerList(), _getSeachBar(), _getIndicator()]);
   }
 }
--- lib/pages/Index/index.dart
+++ lib/pages/Index/index.dart
@@ -4,6 +4,7 @@ import 'package:flutter_shop/pages/Index/components/home_slider.dart';
 import 'package:flutter_shop/pages/Index/components/hot.dart';
 import 'package:flutter_shop/pages/Index/components/more_list.dart';
 import 'package:flutter_shop/pages/Index/components/suggestion.dart';
+import 'package:flutter_shop/viewModels/index.dart';
 
 class IndexView extends StatefulWidget {
   IndexView({Key? key}) : super(key: key);
@@ -17,7 +18,32 @@ class _IndexViewState extends State<IndexView> {
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
-        SliverToBoxAdapter(child: HomeSlider()),
+        SliverToBoxAdapter(
+          child: HomeSlider(
+            bannerList: [
+              BannerItem(
+                id: '1',
+                imgUrl:
+                    'https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/1.jpg',
+              ),
+              BannerItem(
+                id: '2',
+                imgUrl:
+                    'https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/2.png',
+              ),
+              BannerItem(
+                id: '1',
+                imgUrl:
+                    'https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/1.jpg',
+              ),
+              BannerItem(
+                id: '2',
+                imgUrl:
+                    'https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/2.png',
+              ),
+            ],
+          ),
+        ),
         SliverToBoxAdapter(child: SizedBox(height: 10)),
         SliverToBoxAdapter(child: HomeCategory()),
         SliverToBoxAdapter(child: SizedBox(height: 10)),
